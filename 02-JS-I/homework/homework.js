dcddc51e06ebd47233d47d01815e122783e65c7a
frankokmc <ing.marquezcarlos@gmail.com>
2022-03-19 23:56:50 -0300

    mi primer commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = abuelo;
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 20;
+const nuevoNum = 300;
 
 // Crea una variable booleana:
 const nuevoBool = false;
